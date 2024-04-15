ae0331e7f940e6f42765725b70b19d71e47fda30
Your Name <you@example.com>
2024-04-15 23:27:09 +0200

    Webpage Foundation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
 
-Route::get('/', function () {
-    return view('welcome');
+// All listings
+Route::get('/', function ()
+{
+    return view('listings',
+        [
+            'heading' => 'Latest listings',
+            'listings' => Listing::all()
+        ]);
+});
+
+// Single listing
+Route::get('/listings/{listing}', function (Listing $listing)
+{
+    return view('listing',
+        [
+            'listing' => $listing
+        ]
+    );
 });
